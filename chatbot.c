f78e01660508ca5ac13d9d23abee2c46f3dbe4db
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2019-11-11 20:05:33 +0800

    Revert "Small Changes"
    This reverts commit a1c1ec09a16fc47e3f3ea4e21f8df181cacc5827.

--- chatbot.c
+++ chatbot.c
@@ -53,7 +53,9 @@
  * Returns: the name of the chatbot as a null-terminated string
  */
 const char *chatbot_botname() {
+
 	return "Jamie";
+	
 }
 
 
@@ -63,7 +65,9 @@ const char *chatbot_botname() {
  * Returns: the name of the user as a null-terminated string
  */
 const char *chatbot_username() {
+
 	return "User";
+	
 }
 
 
@@ -154,14 +158,8 @@ int chatbot_do_exit(int inc, char *inv[], char *response, int n) {
 int chatbot_is_load(const char *intent) {
 	
 	/* to be implemented */
-	if (strcmp(intent, "load") == 0)
-	{
-		return 1;
-	} 
-	else
-	{
-		return 0;
-	}
+	
+	return 0;
 	
 }
 
@@ -178,7 +176,7 @@ int chatbot_is_load(const char *intent) {
 int chatbot_do_load(int inc, char *inv[], char *response, int n) {
 	
 	/* to be implemented */
-	printf("test that is load is working!!!"); // a test print
+	 
 	return 0;
 	 
 }
